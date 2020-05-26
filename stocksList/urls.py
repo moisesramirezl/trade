e942648504734f1aaaf72d7fdcb0fe2fa9c36a08
moisesramirezl <moises.ramirez@latam.com>
2020-05-26 09:02:59 -0400

    add some example views

--- stocksList/urls.py
+++ stocksList/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
 from . import views
+app_name = 'stocksList'
 
 urlpatterns = [
+    # ex: /stocksList/
     path('', views.index, name='index'),
-]
+    # ex: /stocksList/5/
+    path('<str:user_id>/', views.userStockDetail, name='userStockDetail'),
+    # ex: /stocksList/5/results/
+    path('<str:user_id>/userStockInfo/', views.userStockInfo, name='userStockInfo'),
+]   
--- stocksList/views.py
+++ stocksList/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import UserStocks
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_stocks_list = UserStocks.objects.order_by('-timestamp')[:5]
+    template = loader.get_template('stocksList/index.html')
+    context = {
+        'latest_stocks_list': latest_stocks_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def userStockDetail(request, user_id):
+    return HttpResponse("You're looking at user_id %s." % user_id)
+
+def userStockInfo(request, user_id):
+    response = "You're looking the info of user_id %s."
+    return HttpResponse(response % user_id)
