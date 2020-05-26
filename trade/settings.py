c32fa70e16566573fb2c46174f5027e37fa393ab
moisesramirezl <moises.ramirez@latam.com>
2020-05-26 17:53:44 -0400

    fix db parameters

--- trade/settings.py
+++ trade/settings.py
@@ -93,13 +93,13 @@ pymysql.install_as_MySQLdb()
 if os.getenv('GAE_APPLICATION', None):
     # Running on production App Engine, so connect to Google Cloud SQL using
     # the unix socket at /cloudsql/<your-cloudsql-connection string>
-    #mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
+
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'HOST': '/cloudsql/mysql+pymysql://root:rootpass@/tradedb?unix_socket=/cloudsql/trade-278014:southamerica-east1:tradedb',
+            'HOST': '/cloudsql/trade-278014:southamerica-east1:tradedb',
             'USER': 'root',
-            'PASSWORD': 'rootpass   ',
+            'PASSWORD': 'rootpass',
             'NAME': 'trade',
         }
     }
