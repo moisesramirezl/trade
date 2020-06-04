06b7fc08cdebca7885eceea76539646efd900014
moisesramirezl <moises.ramirez@latam.com>
2020-06-04 18:08:00 -0400

    add debug agent

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from trade.wsgi import application
 
+   try:
+      import googleclouddebugger
+      googleclouddebugger.enable()
+    except ImportError:
+       pass
+
+    import logging
+    logging.basicConfig(level=logging.INFO)
 
 app = application
