191aea82b62474a4ae58d7f1edb7d87edd5978b8
moisesramirezl <moises.ramirez@latam.com>
2020-06-04 12:51:19 -0400

    load stocks in main page, remove link to table

--- stocksList/views.py
+++ stocksList/views.py
@@ -6,18 +6,9 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    latest_stocks_list = StocksList.objects.order_by('-timestamp')[:5]
+    stocksList = StocksList.objects
     template = loader.get_template('stocksList/index.html')
     context = {
-        'latest_stocks_list': latest_stocks_list,
+        'stocksList': stocksList,
     }
     return HttpResponse(template.render(context, request))
-
-
-def userStockDetail(request, user_id):
-    return HttpResponse("You're looking at user_id %s." % user_id)
-
-
-def userStockInfo(request, user_id):
-    response = "You're looking the info of user_id %s."
-    return HttpResponse(response % user_id)
